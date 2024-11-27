465d28cc8effd3005bed6d4548d4bd0517325d22
Владимир Слесарев <santaasus@gmail.com>
2024-11-27 16:15:56 +0400

    change alg to sha512 and change id type

---
+++ config.go
@@ -0,0 +1,38 @@
+package jwttokenhandler
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+const (
+	Refresh = "refresh"
+	Access  = "access"
+)
+
+type TokenClaims struct {
+	ID   string `json:"id"`
+	Type string `json:"type"`
+	jwt.RegisteredClaims
+}
+
+// AppToken is a struct that contains the JWT token
+type AppToken struct {
+	Token          string    `json:"token"`
+	TokenType      string    `json:"type"`
+	ExpirationTime time.Time `json:"expitationTime"`
+}
+
+// TokenTypeKeyName is a map that contains the key name of the JWT in config.json
+var TokenTypeKeyName = map[string]string{
+	Access:  "Secure.JWTAccessSecure",
+	Refresh: "Secure.JWTRefreshSecure",
+}
+
+// Structure likes in config.json
+type SecureConfig struct {
+	JWTAcessSecure     string `json:"JWTAcessSecure"`
+	JWTRefreshSecure   string `json:"JWTRefreshSecure"`
+	JWTAcessTimeMinute int    `json:"JWTAcessTimeMinute"`
+	JWTRefreshTimeHour int    `json:"JWTRefreshTimeHour"`
+}
